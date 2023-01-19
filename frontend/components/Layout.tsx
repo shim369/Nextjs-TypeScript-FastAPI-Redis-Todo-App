347850d57237f48740deae9ea6f978df6d192f05
ohtani shimpei <ohtani.shimpei@gmail.com>
2023-01-19 17:32:23 +0900

    change layout

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -1,13 +1,21 @@
-import Meta from './Meta'
+import Head from "next/head"
 type Props = {
 	children?: React.ReactNode
+	title?: string
+	description?: string
 }
-export default function Layout({ children}: Props) {
+export default function Layout({ children, title, description}: Props) {
 	return(
 		<>
-			<Meta />
+			<Head>
+				<meta name='viewport' content='width=device-width, initial-scale=1' />
+				<meta name='description' content={ description } />
+				<meta charSet='utf-8' />
+				<link rel='icon' href='/favicon.ico' />
+				<title>{ title }</title>
+			</Head>
 			<header>
-				<h1>Next FastAPI App</h1>
+				<h1>{ title }</h1>
 			</header>
 			<main className="main">
 				<div className="container">
@@ -15,8 +23,13 @@ export default function Layout({ children}: Props) {
 				</div>
 			</main>
 			<footer>
-				©2023 Next FastAPI App
+				©2023 { title }
 			</footer>
 		</>
 	)
+}
+
+Layout.defaultProps = {
+	title: 'Next FastAPI App',
+	description: 'Next FastAPI App',
 }
